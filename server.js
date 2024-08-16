49b04639964eca721219d40b79384c56a3699771
mozes <wsbaikaishui@163.com>
2024-08-17 01:45:50 +0800

    update cloudscraper

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const axios = require("axios");
+// const axios = require("axios");
+const cloudscraper = require("cloudscraper");
 // enable CORS
 app.use(cors());
 // set the port on which our app wil run
@@ -12,29 +13,41 @@ app.all('*', (req, res) => {
     if (req.originalUrl.includes('favicon.ico')) {
         res.status(204).end()
     }
-    else if(req.method === 'OPTIONS') {
+    else if (req.method === 'OPTIONS') {
         // CORS Preflight
         res.send();
-    } else{
+    } else {
         let targetURL = req.header('Target-URL');
         // return the data without modification
-        if(!targetURL){
+        if (!targetURL) {
             res.send()
-        }else{
-            axios({
-                url: targetURL+ req.url, 
-                method: req.method, 
-                'Content-Type': 'application/json;charset=UTF-8',
-                "Access-Control-Allow-Origin": "*",
-            }).then((response)=>{
-                if (response.status === 200) {
-                    console.log(response.status);
-                    res.send(response.data);
-                } else {
-                    console.error('Failed to fetchinng holders data:', response.status);
-                    res.send();
-                }
-            });
+        } else {
+            
+         if (targetURL == "https://gmgn.ai") {
+                cloudscraper.get(targetURL + req.url)
+                    .then(response => {
+                        const jsonResponse = JSON.parse(response);
+                         console.log('Response:', jsonResponse.code);
+                        res.send(jsonResponse.data);
+                    })
+                    .catch(error => {
+                        console.error('Error:', error);
+                        res.send();
+                    });
+            } else {
+                cloudscraper.get(targetURL + req.url).then((response) => {
+                         const jsonResponse = JSON.parse(response);
+                         console.log('Response:', jsonResponse);
+                        res.send(jsonResponse);
+                
+                    })
+                    .catch(error => {
+                        console.error('Error:', error);
+                        res.send();
+                    });
+               
+            }
+
         }
     }
 });
